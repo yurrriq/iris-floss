c0bcfcd73d4e7eec655d6f2ffa269280f915a71a
Daniel Georgiev <daniel.georgiev95@gmail.com>
2016-12-26 06:30:46 +0200

    Adding setup file for git

---
+++ setup.sh
@@ -0,0 +1,5 @@
+git config --global user.name "Daniel Georgiev"
+git config --global user.email daniel.georgiev95@gmail.com
+git config --global credential.helper store
+git config --global push.default simple
+git config --global http.postBuffer 524288000
