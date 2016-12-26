7f770164a17f607e4efd7bde08a0745d36956b1c
Daniel Georgiev <daniel.georgiev95@gmail.com>
2016-12-26 13:52:07 +0200

    Update comments

--- iris-floss-src.c
+++ iris-floss-src.c
@@ -1,7 +1,18 @@
-////////////////////////////////////////////////////////////
-// Created by Daniel Georgiev
-// Build with
-//	gcc -o iris-floss iris-floss-src.c -lX11 -lXrandr
+/* Iris floss is free software: you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation, either version 3 of the License, or
+   (at your option) any later version.
+
+   Iris floss is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with Iris floss.  If not, see <http://www.gnu.org/licenses/>.
+
+   Copyright (c) 2017  Daniel Georgiev <daniel@iristech.co>
+*/
 
 ////////////////////////////////////////////////////////////
 #include <X11/extensions/Xrandr.h> 
@@ -183,10 +194,5 @@ int main(int argc, char **argv)
 
 	return 0;
 }
-
-////////////////////////////////////////////////////////////
-
-// Build with
-//	gcc -o iris-floss iris-floss-src.c -lX11 -lXrandr
 
 ////////////////////////////////////////////////////////////
